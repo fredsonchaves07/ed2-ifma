ec639432d204d626673c407cf8dc8e7ac886e820
Fredson Chaves <fredsonchaves07@gmail.com>
2021-02-15 22:25:16 -0300

    Adicionando implementacao de nova unidade

---
+++ estrutura-dados-und03/src/questao02/Principal.java
@@ -0,0 +1,13 @@
+package questao02;
+
+public class Principal {
+    public static void novoConjunto(char[] dados){
+        int aux = dados.length - 1;
+        if(aux)
+    }
+    public static void main(String[] args){
+        char[] dados = {'a', 'b'};
+
+        novoConjunto(dados);
+    }
+}
