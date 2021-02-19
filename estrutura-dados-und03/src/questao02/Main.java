434da32c50bac64fe2a5ba620247994fabaaeff0
Fredson Chaves <fredsonchaves07@gmail.com>
2021-02-19 16:08:44 -0300

    Adicionando arquivo para teste

--- estrutura-dados-und03/src/questao02/Main.java
+++ estrutura-dados-und03/src/questao02/Main.java
@@ -1,9 +1,28 @@
 package questao02;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
+        File arq = new File("entrada.txt");
+
+        try{
+            FileReader fileReader = new FileReader(arq);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String linha = "";
+            while ( ( linha = bufferedReader.readLine() ) != null) {
+                System.out.println(linha);
+            }
+
+            fileReader.close();
+            bufferedReader.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 
         char v[] = {'A', 'B'};
         List<String> newArrayCharacter;
