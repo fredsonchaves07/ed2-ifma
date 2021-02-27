7bac5e570ab258d8ee4e01112bc9dfec4192a5e2
Fredson Chaves <fredsonchaves07@gmail.com>
2021-02-27 14:49:04 -0300

    resolucao final dos desafios

--- estrutura-dados-und03/src/questao02/Main.java
+++ estrutura-dados-und03/src/questao02/Main.java
@@ -4,32 +4,45 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        File arq = new File("entrada.txt");
+        String dir = System.getProperty("user.dir") + "/src/questao02/";
+        File arq = new File(dir + "entrada.txt");
 
         try{
             FileReader fileReader = new FileReader(arq);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String linha = "";
-            while ( ( linha = bufferedReader.readLine() ) != null) {
-                System.out.println(linha);
+            String arqLine ;
+            List<String> fileData = new ArrayList<>();
+
+
+            while ( ( arqLine = bufferedReader.readLine() ) != null) {
+                fileData.add(arqLine);
             }
 
             fileReader.close();
             bufferedReader.close();
+
+            String[] fileCharArray = fileData.get(0).replaceAll(" ", "").split(",");
+            char[] arrayCharacter = new char[fileCharArray.length];
+            int k = new Integer(fileData.get(1));
+            List<String> newArrayCharacter;
+
+            for(int i = 0; i < fileCharArray.length; i++){
+                arrayCharacter[i] = fileCharArray[i].charAt(0);
+            }
+
+            newArrayCharacter = Permutation.permutation(arrayCharacter, k);
+            for(String array : newArrayCharacter){
+                System.out.println(array);
+            }
+
         } catch (IOException e){
             e.printStackTrace();
-        }
-
-        char v[] = {'A', 'B'};
-        List<String> newArrayCharacter;
-        newArrayCharacter = Permutation.permutation(v, 3);
-
-        for(String array : newArrayCharacter){
-            System.out.println(array);
         }
 
     }
